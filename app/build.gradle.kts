493dca3302f2ad16310c332a6dfa92255175c5c5
grzegorzu70-tech <grzegorzu70@gmail.com>
2026-09-18 23:12:39 +0200

    Dodaj app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins { id("com.android.application") }
+android {
+    namespace = "pl.sternikmazury.app"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "pl.sternikmazury.app"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
