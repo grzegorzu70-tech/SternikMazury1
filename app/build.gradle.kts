44c0e931f953a832cc524dd3ff34531273b500d7
grzegorzu70-tech <grzegorzu70@gmail.com>
2026-09-25 12:04:57 +0200

    Android 16 compatibility build 3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins { id("com.android.application") }
 android {
     namespace = "pl.sternikmazury.app"
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         applicationId = "pl.sternikmazury.app"
         minSdk = 26
-        targetSdk = 35
-        versionCode = 31
-        versionName = "3.1"
+        targetSdk = 36
+        versionCode = 32
+        versionName = "3.2"
     }
 }
